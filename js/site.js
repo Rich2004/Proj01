7877c041339233b4d45082f6a951f1dc977c9884
Rich2004 <42697769+Rich2004@users.noreply.github.com>
2019-03-10 11:26:00 -0500

    Made some changes
    Added JS and made form responsive

--- js/site.js
+++ js/site.js
@@ -1,8 +1,20 @@
 // jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
+jQuery(function ($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
-document.getElementById("input-submit").addEventListener("click", function() {
-  alert("Thanks for Subscribing!");
-});
+document.getElementById("submit").addEventListener("click", confirm);
+
+function confirm(event) {
+  for (var i = 0; i < document.getElementsByTagName("input").length; i++) {
+    if (document.getElementsByTagName("input").item(i).value === "" || (document.getElementsByTagName("input").item(i).type === "radio") && (document.getElementsByTagName("input").item(i).checked === false)) {
+      return false;
+    } else {
+      fname = document.getElementById("fname").value;
+      lname = document.getElementById("lname").value;
+      name = document.getElementById("fname").value;
+      alert("Thank you " + fname + " " + lname + " for subscribing.");
+      return true;
+    }
+  }
+}
